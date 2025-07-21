d18d22d5fcf4492f67b63676b0e0dc64020e809e
lfrhh <vortexy.bot.gilu@gmail.com>
2025-07-21 10:04:51 -0300

    update

--- src/js/cmds.js
+++ src/js/cmds.js
@@ -1,79 +1,78 @@
 const userLang = (navigator.language || 'en').toLowerCase();
 const langFallbacks = [userLang, userLang.split('-')[0], 'en'];
+const mark = document.querySelector('.mark');
+const btns = document.querySelectorAll('.btns-category');
 
-function tryLoadLangFile(index = 0) {
+function tryLoadLangFile(index = 0, id) {
   if (index >= langFallbacks.length) {
     document.getElementById('commands-container').textContent = 'Commands not available in this language.';
     return;
   }
   const lang = langFallbacks[index];
   const file = `src/commands_${lang}.json`;
-
+  
   fetch(file)
     .then(res => {
       if (!res.ok) throw new Error();
       return res.json();
     })
-    .then(data => renderCommands(data))
+    .then(data => renderCommands(data, id))
     .catch((err) => { 
-      tryLoadLangFile(index + 1) 
+      tryLoadLangFile(index + 1, id)
     });
 }
 
-function renderCommands(data) {
+function renderCommands(data, category) {
   const container = document.getElementById('commands-container');
   container.innerHTML = '';
-
-  for (const category in data) {
-    const cmds = data[category];
-    const categoryName = cmds.shift();
+  
+  const cmds = data[category];
+  const categoryName = cmds.shift();
+  
+  const title = document.createElement('h2');
+  title.className = "text-red-300 font-bold text-3xl"
+  title.textContent = `${categoryName.replace('(+18)', '⑱')}`;
+  container.appendChild(title);
     
-    const titleDiv = document.createElement('div');
-    titleDiv.className = "category";
-    const title = document.createElement('h2');
-    title.className = "text-red-400 font-bold"
-    title.textContent = `› ${categoryName.replace('(+18)', '⑱')}`;
-    titleDiv.appendChild(title);
-    container.appendChild(titleDiv);
-    
-    titleDiv.addEventListener('click', () => {
-      const isActive = titleDiv.classList.contains('active');
-
-      document.querySelectorAll('.category').forEach(cat =>   {
-        cat.classList.remove('active');
-        const content = cat.querySelector('.categories-content');
-        content.style.height = '0px';
-      });
-
-      if (!isActive) {
-        titleDiv.classList.add('active');
-        titleDiv.querySelectorAll('.categories-content').forEach(ctx => ctx.style.height = ctx.scrollHeight + 'px');
-      } 
+  cmds.forEach(cmd => {
+    const subContainer = document.createElement('div');
+    subContainer.className = "categories-content commands";
+      
+    const cmdName = document.createElement('h2');
+    cmdName.className = "text-red-200 font-bold";
+    cmdName.innerHTML = cmd.command;
+      
+    const cmdDesc = document.createElement('p');
+    cmdDesc.className = "desc text-white";
+    cmdDesc.innerHTML = cmd.desc;
+      
+    const cmdUsage = document.createElement('p');
+    cmdUsage.className = "usage text-white";
+    cmdUsage.innerHTML = `<strong>${cmd.usage}</strong>`;
+      
+    subContainer.appendChild(cmdName);
+    subContainer.appendChild(cmdDesc);
+    subContainer.appendChild(cmdUsage);
+      
+    container.appendChild(subContainer);
   });
-
-    cmds.forEach(cmd => {
-      const subContainer = document.createElement('div');
-      subContainer.className = "categories-content commands";
-      
-      const cmdName = document.createElement('h2');
-      cmdName.className = "text-red-200 font-bold";
-      cmdName.innerHTML = cmd.command;
-      
-      const cmdDesc = document.createElement('p');
-      cmdDesc.className = "desc text-white";
-      cmdDesc.innerHTML = cmd.desc;
-      
-      const cmdUsage = document.createElement('p');
-      cmdUsage.className = "usage text-white";
-      cmdUsage.innerHTML = `<strong>${cmd.usage}</strong>`;
-      
-      subContainer.appendChild(cmdName);
-      subContainer.appendChild(cmdDesc);
-      subContainer.appendChild(cmdUsage);
-      
-      titleDiv.appendChild(subContainer);
-    });
-  }
 }
 
-tryLoadLangFile();
+btns.forEach(x => {
+  x.addEventListener('click', () => {
+    const { offsetLeft, offsetWidth } = x;
+    mark.style.left = offsetLeft + 'px';
+    mark.style.width = offsetWidth + 'px';
+    
+    tryLoadLangFile(0, x.id);
+  })
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+  const first = document.querySelector('.btns-category');
+  const { offsetLeft, offsetWidth } = first;
+  mark.style.left = offsetLeft + 'px';
+  mark.style.width = offsetWidth + 'px';
+  
+  tryLoadLangFile(0, first.id);
+});
